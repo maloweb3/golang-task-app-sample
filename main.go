af160a79145ef07fcc0bfea4218f1e45882fb948
maloweb3 <ueda1023@gmail.com>
2022-12-18 16:23:36 +0900

    ハンドラを作成

--- main.go
+++ main.go
@@ -1,13 +1,19 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
+	"log"
 	"net/http"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, World")
+	http.HandleFunc("/tasks/", func(w http.ResponseWriter, r *http.Request) {
+		taskId := r.URL.Path[len("/tasks/"):]
+		log.Println("taskId:", taskId)
+
+		w.WriteHeader(http.StatusOK)
+		response := map[string]string{"message": "ok"}
+		json.NewEncoder(w).Encode(response)
 	})
 	http.ListenAndServe(":8080", nil)
 }
